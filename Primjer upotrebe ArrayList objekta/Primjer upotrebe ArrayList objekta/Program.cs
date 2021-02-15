1c7025f0ed82c713e311655f604d6370ba1841c8
IvanAbuzimbale <58472137+IvanAbuzimbale@users.noreply.github.com>
2021-02-15 08:31:20 +0100

    Add files via upload

---
+++ Primjer upotrebe ArrayList objekta/Primjer upotrebe ArrayList objekta/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Primjer_upotrebe_ArrayList_objekta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList al = new ArrayList();
+            al.Add(4);
+            al.Add("C# if .NET");
+            al.Insert(1, "Programiranje");
+
+            Console.WriteLine("-- Elementi kolekcije na početku: ");
+            for (int i= 0; i < al.Count; i++)
+            {
+                Console.WriteLine(al[i]);
+            }
+            if (al.Contains(4))
+            {
+                al.Remove(4);
+            }
+            int pozicija = al.IndexOf("Programiranje");
+            if (pozicija >= 0)
+            {
+                al.RemoveAt(pozicija);
+            }
+
+            Console.WriteLine("\n-- Elementi kolekcije na završetku: ");
+
+            for (int i =0; i < al.Count; i++)
+            {
+                Console.WriteLine(al[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
